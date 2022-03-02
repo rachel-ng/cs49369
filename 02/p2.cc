2e5f1cfafd71f40658a5081dc062bdef70fcd3b3
Rachel Ng <rachel.ng.135@gmail.com>
2022-03-02 15:22:45 -0500

    done - working on improvements

--- 02/p2.cc
+++ 02/p2.cc
@@ -7,39 +7,27 @@ p2 -
 */
 
 #include "image.h"
+#include "util.h"
 #include <cstdio>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <unordered_map>
 #include <set>
+#include <unordered_map>
 
 
 using namespace std;
 using namespace ComputerVisionProjects;
 
-void printVint(vector<int> v) {
-    // cout << "vec<int>\t"; 
-    for (const int &i : v) {
-        cout << i << "  ";
-    }
-}
-
-void printSint(set<int> s) {
-    // cout << "set<int>\t";  
-    for (const int i : s) {
-        cout << i << "  ";
-    }
-}
 
 void ImageColors(Image *img) {
-    bool debug = true;
+    bool debug = false;
 
     const int rows = img->num_rows();
     const int cols = img->num_columns();
 
     const int shades = img->num_gray_levels();
-    int increments = 200 / shades; 
+    int increments = 100 / shades; 
     set<int> colors;
 
     for (int i = 0; i < rows; i++) {
@@ -57,9 +45,7 @@ void ImageColors(Image *img) {
 
     if (debug) {
         cout << img->num_rows() << " " << img->num_columns() << "\t" << img->num_gray_levels() << endl;
-        cout << "COLORS\t\t" ;
-        printSint(colors);
-        cout << endl;
+        cout << "COLORS\t\t" << colors << endl;
     }
 }
 
@@ -67,7 +53,7 @@ vector<int> NeighborhoodColors(Image *img, int px, int py) {
     // INPUTS   image, (x,y) coords of a pixel
     // RETURNS  vector with the color of the neighborhood of a cell
     
-    bool debug = true;
+    bool debug = false;
 
     const int rows = img->num_rows();
     const int cols = img->num_columns();
@@ -92,8 +78,7 @@ vector<int> NeighborhoodColors(Image *img, int px, int py) {
     }
 
     if (debug && !equal(nbh_colors.begin() + 1, nbh_colors.end(), nbh_colors.begin())) { 
-        cout << "NEIGHBORS\t";
-        printVint(nbh_colors);
+        cout << "NEIGHBORS\t" << nbh_colors;
     }
 
     return nbh_colors;
@@ -103,6 +88,7 @@ vector<int> NeighborhoodColors(Image *img, int px, int py) {
 void ConnectedComponents(Image *img, unordered_map<int, set<int>> &equivalence) {
     if (img == nullptr) abort();
     
+    bool debug = false; 
     const int rows = img->num_rows();
     const int cols = img->num_columns();
 
@@ -113,20 +99,19 @@ void ConnectedComponents(Image *img, unordered_map<int, set<int>> &equivalence) 
             int pixel_color = img->GetPixel(i,j);
 
             if (pixel_color != 0) { 
-                cout << endl << "(" << i << ", " << j << ")\t" << pixel_color << endl;
+                if (debug) cout << "(" << i << ", " << j << ")\t" << pixel_color << endl;
                 vector<int> nbh_colors = NeighborhoodColors(img, i, j);
-                // cout << endl; 
 
                 int pixel_label = nbh_colors.size() > 1 ? *min_element(nbh_colors.begin(), nbh_colors.end()) : 255;
 
                 if (pixel_label == 255) {
-                    cout << "PIXEL LABEL\t" << pixel_label;
+                    if (debug) cout << "PIXEL LABEL\t" << pixel_label;
                     labels++;
                     pixel_label = labels;
                     set<int> equiv {pixel_label};
                     equivalence[pixel_label] = equiv; 
 
-                    cout << " -> " << pixel_label << endl;
+                    if (debug) cout << " -> " << pixel_label << endl;
                 }
 
                 img->SetPixel(i,j,pixel_label);
@@ -139,7 +124,6 @@ void ConnectedComponents(Image *img, unordered_map<int, set<int>> &equivalence) 
                     }
                 }
 
-                cout << endl;
             }
         }
     }
@@ -171,7 +155,7 @@ int main(int argc, char **argv){
 
     unordered_map<int,set<int>> equivalent_labels;
 
-    cout << "PASS 1" << endl << endl;
+    if (debug) cout << "PASS 1" << endl;
     ConnectedComponents(&an_image, equivalent_labels); 
     // ImageColors(&an_image);
     if (!WriteImage("1_" + output_file, an_image)){
@@ -180,7 +164,7 @@ int main(int argc, char **argv){
     }
 
 
-    cout << "PASS 2" << endl << endl;
+    if (debug) cout << "PASS 2" << endl;
     ConnectedComponents(&an_image, equivalent_labels); 
     ImageColors(&an_image);
     if (!WriteImage(output_file, an_image)){
@@ -189,13 +173,7 @@ int main(int argc, char **argv){
     }
 
 
-    if (false) {
-        for (auto const &i : equivalent_labels) {
-            cout << i.first << "\t"; 
-            printSint(i.second); 
-            cout << endl;
-        }
-    }
+    if (false) cout << equivalent_labels << endl;
 
     if (!ReadImage("1_" + output_file, &an_image)) {
         cout <<"Can't open file " << "1_" + input_file << endl;
