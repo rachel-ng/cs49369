fc40996948b577b3cb1d0347e1ff2b3356ce517a
Rachel Ng <rachel.ng.135@gmail.com>
2022-03-02 00:09:12 -0500

    p1

---
+++ 02/p1.cc
@@ -0,0 +1,59 @@
+/* 
+
+Rachel Ng 
+
+p1 - converts gray-level image to binary  
+threshold used was 115 
+
+*/
+
+#include "image.h"
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+using namespace std;
+using namespace ComputerVisionProjects;
+
+
+void ThreshImage (Image *img, int threshold) {
+    if (img == nullptr) abort();
+    
+    const int rows = img->num_rows();
+    const int cols = img->num_columns();
+    
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++){ 
+            img->SetPixel(i,j, img->GetPixel(i,j) < threshold ? 0 : 255);
+        }
+    }
+
+    img->SetNumberGrayLevels(1);
+}
+
+int main(int argc, char **argv){
+  
+    if (argc!=4) {
+        printf("Usage: %s file1 thresh file2\n", argv[0]);
+        return 0;
+    }
+
+    const string input_file(argv[1]);
+    const int threshold(stoi(argv[2]));
+    const string output_file(argv[3]);
+
+    Image an_image;
+
+    if (!ReadImage(input_file, &an_image)) {
+        cout <<"FILE " << input_file << endl;
+        return 0;
+    }
+
+    ThreshImage(&an_image, threshold); 
+  
+    if (!WriteImage(output_file, an_image)){
+        cout << "FILE " << output_file << endl;
+        return 0;
+    }
+}
+
